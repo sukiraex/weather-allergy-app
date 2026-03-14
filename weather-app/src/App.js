1042e4e6c33715a76b623d5ae9a22dfa6761fcd0
Sunny-1118 <dhtjsghk1118@gmail.com>
2026-03-14 17:09:45 +0000

    Add weather widgets to App

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,18 @@
 import WeatherCard from './components/wwidget/wwidget';
+import WindWidget from './components/wwidget/widgets/WindWidget';
+import HumidityWidget from './components/wwidget/widgets/HumidityWidget';
+import UVWidget from './components/wwidget/widgets/UVWidget';
+import SunsetWidget from './components/wwidget/widgets/SunsetWidget';
 
 function App() {
   return (
     <div style={{ padding: '40px', backgroundColor: '#b8d4e8', minHeight: '100vh' }}>
       <WeatherCard city="London" />
+
+      <SunsetWidget />
+      <WindWidget />
+      <HumidityWidget />
+      <UVWidget />  
     </div>
   );
 }
