fde25a6629cc3224d1a7cc10d4a2d62dd4b2a35c
SumayaM <epicgamer50012@gmail.com>
2026-03-28 23:09:23 +0000

    added symptoms to rate severity

--- weather-app/src/components/wwidget/LogSymptoms.jsx
+++ weather-app/src/components/wwidget/LogSymptoms.jsx
@@ -1,16 +1,53 @@
 import { theme } from "../../theme";
+import { useState } from "react";
+
+const symptoms = ["Sneezing", "Runny Nose", "Itchy Eyes", "Congestion", "Headache", "Fatigue"];
+
 
 export default function LogSymptoms(){
+    const rating = useState([0,0,0,0,0,0]);
     return (
+        
         <div style={{...theme.card, backgroundColor: '#FFFFFF', justifyContent: 'space-between', overflow: 'hidden', padding: 0, }}>
             <div style={{background: theme.buttons.buttongradient, padding: '24px 24px', display: 'flex', alignItems: 'center', gap: '1px', maxWidth: '100%', color: '#FFFFFF', flexDirection: 'column'}}>
-                <h4 style={{margin: 0, textAlign: 'left', float: 'left', fontWeight: '400',}}>Log Symptoms</h4>
-                <h5 style={{margin: 0, textAlign: 'left', float: 'left', fontWeight: '400'}}>Track how you're feeling</h5>
+                <h4 style={{margin: 0, float: 'left', fontWeight: '400',}}>Log Symptoms</h4>
+                <h5 style={{margin: 0, float: 'left', fontWeight: '400'}}>Track how you're feeling</h5>
             </div>
-            <button style={{...theme.buttons, background: '#F3F4F6', width:'50%', height: '40px', borderRadius: '14px', color:'#364153' }}>Cancel</button>
 
-            <button style={{...theme.buttons, background: theme.buttons.buttongradient, width:'50%', height: '40px', borderRadius: '14px' }}>Log Symptoms</button>
 
+      
+            <div style={{overflowY: 'scroll'}}>
+                {
+                    
+                    symptoms.map((i) => (
+                        <div key={i}>
+
+                            <div style={{justifyContent: 'space-between'}}>
+                            
+                            
+                            <label>
+                                {i}
+                            </label>
+                            <button>+</button>
+
+                            </div>
+                           
+                        </div>
+                    ))
+                }
+
+
+            </div>
+            
+
+
+
+
+            <div style={{padding: '10px', display: 'flex', justifyContent: 'space-between'}}>
+            <button style={{...theme.buttons, background: '#F3F4F6', width:'48%', height: '40px', borderRadius: '14px', color:'#364153' }}>Cancel</button>
+            <button style={{...theme.buttons, background: theme.buttons.buttongradient, width:'48%', height: '40px', borderRadius: '14px' }}>Log Symptoms</button>
+            </div>
+           
         </div>
     );
 
